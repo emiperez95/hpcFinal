3574d483a03ab25c5378295b371da2fbe143d6c5
Emiliano Perez <emiliano.perez@fing.edu.uy>
2019-07-10 09:58:19 -0300

    Removed testing files

--- controllers/logger.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-class Logger():
-    def __init__(self, cc):
-        self.rank = cc["mpi"]["rank"]
-
-    def to_log(self, message):
-        print("{}) ".format(self.rank) + message)
-        sys.stdout.flush()
